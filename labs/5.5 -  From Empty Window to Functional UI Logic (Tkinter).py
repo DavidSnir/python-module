2ff6fe94edd57ea2fe45797d6858bdbaf372e792
DavidSnir <david.snir@gmail.com>
2026-03-30 22:01:17 +0300

    finished 5.5 lab

--- labs/5.5 -  From Empty Window to Functional UI Logic (Tkinter).py
+++ labs/5.5 -  From Empty Window to Functional UI Logic (Tkinter).py
@@ -1,12 +1,24 @@
 import tkinter as tk
 
+
+def talk_back():
+    user_answer=user_entry.get()
+    answer.config(text=f"Yea, I'm {user_answer} too")
+
 #1
 root = tk.Tk()
-root.title("test")
+root.title("The Greeter")
+root.geometry("300x200")
+
+question = tk.Label(root,text="Hello, how are you?")
+answer = tk.Label(root,text="", fg="green")
+user_entry = tk.Entry(root,width=300)
+done_button = tk.Button(root, text="Done!",command=talk_back)
 
 
-
-
-
+question.pack()
+user_entry.pack()
+done_button.pack()
+answer.pack()
 
 root.mainloop()

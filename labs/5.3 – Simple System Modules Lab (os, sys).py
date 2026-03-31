28b811f7dccc19f6f5a8e6f16056bf877385afe5
DavidSnir <david.snir@gmail.com>
2026-03-31 13:07:03 +0300

    Add initial implementation for Simple System Modules Lab and Exploring More System & Utility Modules

---
+++ labs/5.3 – Simple System Modules Lab (os, sys).py
@@ -0,0 +1,38 @@
+import os as os
+import sys as sys
+
+#1
+# print(os.name)
+
+# #2
+# print(os.getcwd())
+
+# #3
+# print(os.listdir())
+
+# #4
+# os.mkdir("test_folder")
+
+# #5
+# os.chdir("test_folder")
+# print(os.getcwd())
+
+# #6
+# with open("notes.txt","w") as file:
+#     file.write("New file")
+
+# #7
+# print(os.path.exists("./notes.txt"))
+
+# #8
+# os.remove("notes.txt")
+
+# #9
+# os.chdir("..")
+
+# #10
+# os.rmdir("test_folder")
+
+# -----------------------------------------------------------------------
+
+#11
